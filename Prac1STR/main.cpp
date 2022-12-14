845cf08cea8006802c45f51d2ac46f30e4f70207
Miquel Medina <miquel.medina@estudiants.urv.cat>
2022-12-14 15:13:50 +0100

    thread lcd done

--- Prac1STR/main.cpp
+++ Prac1STR/main.cpp
@@ -1,52 +1,58 @@
 #include "Grove_LCD_RGB_Backlight.h"
+
 #include "mbed.h"
+
+#include <string>
 
 
 AnalogIn lightSensor(A0);
 PwmOut led(D3);
 Grove_LCD_RGB_Backlight rgbLCD(D14, D15);
+Thread thread;
+float lux;
 
 float counts_ls = 0;
-char currentLux[] = {'l',':',' ',' ',' ',' ','%','d',':',' ',' ',' ',' ','%',' '}; //l: 3 d: 3 --- 8 100% 100%
-float calculate_lux(float counts){
-    counts = (((5*500)*counts)-500)/10;
-    return ( counts < 0 ? 0 : counts);
+float calculate_lux(float counts) {
+  counts = (((5 * 500) * counts) - 500) / 10;
+  if (counts > 200){
+      counts = 200;
+  }
+
+  return (counts < 0 ? 0 : counts);
+}
+
+void print_lcd () {
+    while(true){
+        string message = "LUX: " + std::to_string(lux/2) + "%" + "LUX: " + std::to_string(lux/2) + "%";
+        char output[message.length() + 1];
+        strcpy(output, message.c_str());
+
+        rgbLCD.clear();
+        rgbLCD.print(output);
+        ThisThread::sleep_for(500ms);
+    }
 }
 
 // main() runs in its own thread in the OS
-int main()
-{
-    rgbLCD.setRGB(154,255,255);
-    bool cond = true;
-    while (true) {
-        //LCD
-        if(cond) {
-           rgbLCD.writech('H');
-           rgbLCD.writech('e');
-           rgbLCD.writech('l');
-           rgbLCD.writech('l');
-           rgbLCD.writech('o');
-           rgbLCD.writech(' ');
-           rgbLCD.writech('W');
-           rgbLCD.writech('o');
-           rgbLCD.writech('r');
-           rgbLCD.writech('l');
-           rgbLCD.writech('d');
-           rgbLCD.writech('!');
-           cond = false;
-        }
-
-        counts_ls = lightSensor.read();
-        for (int i = 0; i<100; i++) {
-            counts_ls = counts_ls + lightSensor.read();
-        }
-        counts_ls = counts_ls / 100;
-        //printf("Counts: %f\n", counts_ls);
-        float lux = calculate_lux(counts_ls);
-        printf("Lux: %f\n", lux);
-        
-        led.period(0.02f);
-        led.write(1-lux/200);   //Lux is our max. lux/200 = 0-1
-        ThisThread::sleep_for(5ms);
+int main() {
+  thread.start(print_lcd);
+  while (true) {
+    counts_ls = lightSensor.read();
+    for (int i = 0; i < 100; i++) {
+      counts_ls = counts_ls + lightSensor.read();
     }
+    counts_ls = counts_ls / 100;
+    //printf("Counts: %f\n", counts_ls);
+    lux = calculate_lux(counts_ls);
+    printf("Lux: %f\n", lux);
+
+    led.period(0.02f);
+    led.write(1 - lux / 200); //Lux is our max. lux/200 = 0-1
+
+    rgbLCD.setRGB(154, 255, 255);
+
+
+
+    ThisThread::sleep_for(5ms);
+  }
 }
