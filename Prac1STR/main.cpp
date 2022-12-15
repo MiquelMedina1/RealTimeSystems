26d2d6318459a91dccf6039b3a62a00043116d3a
Angel Gascon <angel.gascon@estudiants.urv.cat>
2022-12-15 22:41:33 +0100

    initial planification added and potentiometer, correct deadline and planification needed

--- Prac1STR/main.cpp
+++ Prac1STR/main.cpp
@@ -6,17 +6,22 @@
 #include <cstdio>
 #include <string>
 
-AnalogIn potenciometer(A1);
+#define DEADLINE 600
+
+AnalogIn potentiometer(A1);
 AnalogIn lightSensor(A0);
 PwmOut buzzer(D5);
 PwmOut led(D3);
 InterruptIn button(D2);
 Grove_LCD_RGB_Backlight rgbLCD(D14, D15);   //Connected to I2C
-Thread thread;
-float lux;
+
+uint64_t timer_lux_mean;
+uint64_t main_timer;
+float pot_mesurement;
+float lux, lux_compensate;
 float lux_mean, n_lux_means;
 bool do_lux_mean = false;
-uint64_t timer_lux_mean;
+bool buzzer_active = false;
 
 float calculate_lux() {
     
@@ -32,21 +37,32 @@ float calculate_lux() {
     
     if (counts > 1)
         counts = 1;
-    if (counts < 0) //This should be tested by buzzer
+    if (counts < 0)
         counts = 0;
     
     return counts;
 }
 
 void read_and_comp_lux() {
-    //Kernel::get_ms_count();
     uint64_t start_read_and_comp_lux = get_ms_count();
     lux = calculate_lux();
-
+    pot_mesurement = potentiometer.read();
     
-    led.write(1-lux);
+    if(pot_mesurement < 0) {
+        buzzer.write(0.25);
+        buzzer_active = true;
+        pot_mesurement = 0;
+    }
 
-    printf("%llu\n", get_ms_count() - start_read_and_comp_lux);
+    if (lux <= pot_mesurement)
+        lux_compensate = pot_mesurement - lux;
+    else
+        lux_compensate = 0;
+    
+    led.write(lux_compensate-lux);
+
+
+    printf("Time: %llu //// Potentiometer: %f\n", get_ms_count() - start_read_and_comp_lux, pot_mesurement);
 }
 
 void print_lcd() {
@@ -58,7 +74,7 @@ void print_lcd() {
     rgbLCD.print(output);
 
     rgbLCD.locate(0, 1);
-    message = "RES: " + std::to_string((1 - lux)*100) + "%";
+    message = "RES: " + std::to_string(lux_compensate*100) + "%";
     char output2[message.length() + 1];
     strcpy(output2, message.c_str());
     rgbLCD.print(output2);
@@ -68,7 +84,7 @@ void calculate_lux_mean() {
     lux_mean += lux;
     n_lux_means++;
     
-    if((get_ms_count() - timer_lux_mean) >= 10000){
+    if((get_ms_count() - timer_lux_mean) >= 10000) {
 
         printf("Last 10s lux: %f:%f:%f", lux_mean, n_lux_means, lux_mean/n_lux_means*100);
 
@@ -81,7 +97,7 @@ void calculate_lux_mean() {
         rgbLCD.print(output);
 
         rgbLCD.locate(0, 1);
-        message = std::to_string((lux_mean)*100) + "%";
+        message = std::to_string((lux_mean/n_lux_means)*100) + "%";
         char output2[message.length() + 1];
         strcpy(output2, message.c_str());
         rgbLCD.print(output2);
@@ -89,19 +105,26 @@ void calculate_lux_mean() {
             
         do_lux_mean = false;
         button.enable_irq();
+        ThisThread::sleep_for(1s);      //Be able to see result
     }
 }
 
 void button_click() {
-    button.disable_irq();
+    button.disable_irq();   //We disable interrupts to avoid conflicts until the mean calculus is finished
     lux_mean = 0;
     n_lux_means = 0;
     do_lux_mean = true;
     timer_lux_mean = get_ms_count();
 }
 
-void test_deadline(){
-
+bool test_deadline() {
+    if (DEADLINE < (get_ms_count() - main_timer)) {
+        buzzer.write(0.25);
+        buzzer_active = true;
+        ThisThread::sleep_for(250ms);
+        return true;
+    }
+    return false;
 }
 
 int main() {
@@ -112,10 +135,25 @@ int main() {
 
   while (true) {
     
+    main_timer = get_ms_count();
     read_and_comp_lux();
-    print_lcd();
-    if(do_lux_mean)
-        calculate_lux_mean();
-    ThisThread::sleep_for(500ms);
+    
+    if(!test_deadline()) {
+        
+        print_lcd();
+
+        if(do_lux_mean)
+            calculate_lux_mean();
+
+        if(!test_deadline()) {
+            uint64_t time_left = DEADLINE - (main_timer - get_ms_count());
+            ThisThread::sleep_for(time_left);
+        }
+
+    }
+    if(buzzer_active) {
+        buzzer.write(0.0);
+        buzzer_active = false;
+    }
   }
 }
